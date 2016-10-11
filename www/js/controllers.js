3cfeb1a82146e329e02f430bbcd52ae7e614249f
klenzo <crea2luxe@gmail.com>
2016-10-11 16:47:48 +0200

    Correction de bug, oublie de ','

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -12,14 +12,14 @@ angular.module('starter.controllers', [])
 			img: 'img/slide.jpg', 
 			short: 'Petite Description', 
 			descript: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-		}
+		},
 		{ 
 			title: 'Klenzo2', 
 			id: 1, 
 			img: 'img/slide.jpg', 
 			short: 'Petite Description', 
 			descript: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-		}
+		},
 		{ 
 			title: 'Klenzo2', 
 			id: 1, 
